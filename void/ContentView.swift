44aee6004687e7a4f5a7f9e04178fa04ca41ac8c
santi-ug <santi.uribegil@gmail.com>
2026-03-14 12:50:06 -0500

    Initial Commit

---
+++ void/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  void
+//
+//  Created by Santiago Uribe on 3/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ void/voidApp.swift
@@ -0,0 +1,17 @@
+//
+//  voidApp.swift
+//  void
+//
+//  Created by Santiago Uribe on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct voidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
